de7011f96bf3c4ae405cedd3441759000384ad91
Makhosandile Ndondo <makhosi@Makhosandiles-MacBook-Air.local>
2023-03-16 15:03:01 +0200

    Thu 16 Mar 2023 15:03:01 SAST - Deploy with updated certificates

---
+++ proxy/index.php
@@ -0,0 +1,48 @@
+
+<?php
+$headers = apache_request_headers();
+$headers_native = array();
+$body = file_get_contents('php://input');
+
+$scheme = "http";
+$server_name = "209.97.183.61";
+$server_port = 8003;
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request_uri = $_SERVER['REQUEST_URI'];
+$url = $scheme."://".$server_name.$request_uri;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_PORT, $server_port);
+$headers['Host'] = $server_name;
+$headers['Content-Length'] = strlen($body);
+foreach($headers as $key=>$value)
+{
+	$headers_native[] = $key.":"." ".$value;
+}
+if($method == 'GET')
+{
+	curl_setopt($ch, CURLOPT_POST, false);
+}
+else if($method == 'POST')
+{
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+}
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers_native );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true );
+$result = curl_exec($ch);
+curl_close($ch );
+$response = explode("\r\n\r\n", $result, 2);
+$header_lines = explode("\r\n", $response[0]);
+foreach($header_lines as $line)
+{
+	header($line);
+}
+if(isset($response[1]))
+{
+	echo $response[1];
+}
+?>
